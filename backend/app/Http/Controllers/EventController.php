bad6ae6c8bb30cfe99f7f89fb8f7fc371f658920
dakshub <sainidak@gmail.com>
2023-09-04 00:17:01 -0400

    Add event update method in controller

--- backend/app/Http/Controllers/EventController.php
+++ backend/app/Http/Controllers/EventController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\EventUpdateRequest;
 use App\Http\Resources\EventResource;
+use App\Models\Event;
 use App\Services\EventService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
@@ -20,5 +23,15 @@ class EventController extends Controller
         $events = $this->eventService->getUserEvents($request->user());
 
         return EventResource::collection($events);
+    }
+
+    public function update(EventUpdateRequest $request)
+    {
+        $eventId = $request->route('id');
+        $requestArray = json_decode($request->getContent(), true);
+        $eventResponse = $this->eventService->updateEvent($eventId, $requestArray);
+
+        return response()
+            ->json($eventResponse, JsonResponse::HTTP_OK);
     }
 }
