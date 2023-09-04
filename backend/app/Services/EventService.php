a728756e1dc6b10894a2af0bd08ee25d3628a150
dakshub <sainidak@gmail.com>
2023-09-04 00:19:07 -0400

    Use EventService to call repository for updating

--- backend/app/Services/EventService.php
+++ backend/app/Services/EventService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Models\Event;
 use App\Models\User;
 use App\Repositories\EventRepository;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
@@ -19,5 +20,10 @@ class EventService
     public function getUserEvents(User $user): LengthAwarePaginator
     {
         return $this->eventRepository->getLatestPaginatedUserEvents($user, self::EVENTS_PER_PAGE);
+    }
+
+    public function updateEvent(int $eventId, array $eventArray): Event
+    {
+        return $this->eventRepository->updateEvent($eventId, $eventArray);
     }
 }
