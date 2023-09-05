8fcf45cb46505275b35e30c0930c19720d594281
dakshub <sainidak@gmail.com>
2023-09-05 02:24:54 -0400

    Add SubscriberRepo to get Subcriber analytics

---
+++ backend/app/Repositories/SubscriberRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Subscriber;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+
+class SubscriberRepository
+{
+    public function getRevenueMadeFromSubscriptions(User $user, int $days): float
+    {
+        return Subscriber::where('user_id', $user->id)
+            ->where('created_at', '>=', now()->subDays($days))
+            ->select(DB::raw('
+                    SUM(CASE WHEN subscription_tier = 1 THEN 5
+                    WHEN subscription_tier = 2 THEN 10
+                    WHEN subscription_tier = 3 THEN 15
+                    ELSE 0 END) AS subscription_revenue
+            '))
+            ->value('subscription_revenue');
+    }
+}
